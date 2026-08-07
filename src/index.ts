9ad486e6acabdf4e6bc8f1b0952fdb75ce53e9d3
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-08-07 14:19:33 +0530

    data add

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+import { router } from "./trpc.js";
+import { userRouter } from "./routers/user.router.js";
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (_req, res) => {
-  res.send("tRPC Backend is running");
+const appRouter = router({
+  user: userRouter,
 });
+
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+  }),
+);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+export type AppRouter = typeof appRouter;
---
+++ src/routers/user.router.ts
@@ -0,0 +1,23 @@
+import { router, publicProcedure } from "../trpc.js";
+
+export const userRouter = router({
+  getAll: publicProcedure.query(() => {
+    return [
+      {
+        id: 1,
+        name: "Chanchal",
+        email: "chanchal@email.com",
+      },
+      {
+        id: 2,
+        name: "Rahul",
+        email: "rahul@email.com",
+      },
+      {
+        id: 3,
+        name: "Rohit",
+        email: "rohit@email.com",
+      },
+    ];
+  }),
+});
